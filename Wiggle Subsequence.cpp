b3664eb59d883f85ce9fe2cc6d9da6994f30e3d1
Animesh  Rathore <animeshrathore255@gmail.com>
2025-07-07 15:23:49 +0530

    Update Wiggle Subsequence.cpp

--- Wiggle Subsequence.cpp
+++ Wiggle Subsequence.cpp
@@ -4,6 +4,10 @@ public:
         if(nums.size() < 2) return nums.size();
         int up = 1, down = 1;
 
+       // up: the length of the longest wiggle subsequence that ends in an upward difference (i.e., increasing).
+
+// down: the length of the longest wiggle subsequence that ends in a downward difference (i.e., decreasing)."
+
         for(int i=1;i<nums.size();i++){
             if(nums[i] > nums[i-1]) up = down + 1;
             else if(nums[i] < nums[i-1]) down = up + 1;
