a606a6fecc40e7307d14393b32d1cd270a9ab5c4
Animesh  Rathore <animeshrathore255@gmail.com>
2025-07-11 11:26:24 +0530

    Update ValidParenthesis.cpp

--- ValidParenthesis.cpp
+++ ValidParenthesis.cpp
@@ -21,3 +21,24 @@ public:
         return open.empty();
     }
 };
+
+// greedy approach TC:- O(n), SC:- O(1)
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int open = 0, close = 0;
+        int len = s.length() - 1;
+
+        for(int i=0;i<=len;i++){
+            if(s[i] == '(' || s[i] == '*') open++;
+            else open--;
+
+            if(s[len - i] == ')' | s[len - i] == '*') close++;
+            else close--;
+
+            if(open < 0 | close < 0) return false;
+        }
+
+        return true;
+    }
+};
