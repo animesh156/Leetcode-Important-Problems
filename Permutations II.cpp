2f3fa2f6886e7b8ac238c66b678bd5d5e374bc7f
Animesh  Rathore <animeshrathore255@gmail.com>
2025-07-12 15:03:24 +0530

    Update Permutations II.cpp

--- Permutations II.cpp
+++ Permutations II.cpp
@@ -24,3 +24,37 @@ public:
         return res;
     }
 };
+
+
+
+
+
+class Solution {
+public: 
+    void f(vector<int>& nums, vector<vector<int>> &res, vector<int> &cur,vector<bool> &vis){
+        if(cur.size() == nums.size()) {
+            res.push_back(cur);
+            return;
+        }
+
+        for(int i=0;i<nums.size();i++){
+            if(vis[i]) continue;
+            if(i > 0 && nums[i] == nums[i-1] && !vis[i-1]) continue;
+            vis[i] = true;
+            cur.push_back(nums[i]);
+            f(nums,res,cur,vis);
+            cur.pop_back();
+            vis[i] = false;
+        }
+
+    }
+       
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> res;
+        vector<int> cur;
+        sort(nums.begin(),nums.end());
+        vector<bool> vis(nums.size(),false);
+        f(nums,res,cur,vis);
+        return res;
+    }
+};
