54af97c595e31ed15073b414ca9bb7af13ba5044
Animesh  Rathore <animeshrathore255@gmail.com>
2025-07-18 22:58:20 +0530

    Update Find the Smallest Divisor Given a Threshold.cpp

--- Find the Smallest Divisor Given a Threshold.cpp
+++ Find the Smallest Divisor Given a Threshold.cpp
@@ -24,3 +24,20 @@ public:
         return ans;
     }
 };
+
+
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int l = 0, h = arr.size()-1;
+
+        while(l <= h) {
+            int mid = l + (h-l) / 2;
+            int missing = arr[mid] - (mid + 1); //missing no till index i, is arr[i] - (i+1)
+            if(missing < k) l = mid + 1;
+            else h = mid - 1;
+        }
+
+        return l + k;
+    }
+};
