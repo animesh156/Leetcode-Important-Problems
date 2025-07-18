60177d298e81b14b7e572feb7b270b78e8457a45
Animesh  Rathore <animeshrathore255@gmail.com>
2025-07-18 22:49:29 +0530

    Create Find the Smallest Divisor Given a Threshold.cpp

---
+++ Find the Smallest Divisor Given a Threshold.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool possible(vector<int>& nums, int tar, int k) {
+        int sum = 0;
+        for(auto num : nums) {
+            sum += ceil((double)num / (double)k);
+        }
+        return sum <= tar;
+    }
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        int h = *max_element(nums.begin(),nums.end());
+        int l = 1;
+        int ans = 0;
+        while(l <= h) {
+            int mid = l + (h - l) / 2;
+            if(possible(nums,threshold,mid)) {
+               ans = mid;
+               h = mid - 1;
+            }
+            else {
+                l = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
